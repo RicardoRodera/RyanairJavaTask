fbffe46a53024b86070b9291f055f55a770045e8
Ricardo Rodera Álvarez <jew16373@educastur.es>
2023-09-11 16:52:29 +0200

    Creation of the Spring project

---
+++ src/main/java/com/example/RyanairJavaTask/RyanairJavaTaskApplication.java
@@ -0,0 +1,13 @@
+package com.example.RyanairJavaTask;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RyanairJavaTaskApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RyanairJavaTaskApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/RyanairJavaTask/RyanairJavaTaskApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.RyanairJavaTask;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RyanairJavaTaskApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
